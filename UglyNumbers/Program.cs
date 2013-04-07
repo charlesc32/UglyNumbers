7568d21ff4dbb70a883b1106c5705d0f42abbd37
ccymbor <ccymbor@gmail.com>
2013-04-06 17:12:02 -0700

    init

---
+++ UglyNumbers/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var input = new Dictionary<int, string>();
+        int count = 0;
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+            input.Add(count, line);
+            count++;
+        }
+
+        var output = new ConcurrentDictionary<int, string>();
+        Parallel.ForEach(input, i=> 
+        {
+            output.TryAdd(i.Key, FindUglyNumbers(i.Value));   
+        });
+
+        foreach (var item in output.OrderBy(i => i.Key))
+        {
+            Console.WriteLine(item.Value);
+        }
+    }
+
+    private static string FindUglyNumbers(string input)
+    {
+        var strippedInput = (input.Length > 1) ? input.TrimStart('0') : input;
+        if (input[0] == '0' && input.Length > 1) strippedInput = "0" + strippedInput;
+
+        var numOfZerosStripped = input.Length - strippedInput.Length;
+        var missingPossibilities = Math.Pow(3, numOfZerosStripped);
+
+        var basePossibilities = CreateBasePossibilities(strippedInput);
+        var allPossibleCombinations = CreatePossibilities(basePossibilities);
+        
+        int numOfUglies = allPossibleCombinations.AsParallel().Sum(possibility => EvaluatesToUgly(possibility));
+                
+        return (numOfUglies * missingPossibilities).ToString();
+    }
+
+    private static int EvaluatesToUgly(string possibleUglyNum)
+    {
+        double result = 0;
+        var currentNum = new StringBuilder();
+
+        for (int i = 0; i < possibleUglyNum.Length; i++)
+        {
+            if (possibleUglyNum[i] == '+' || possibleUglyNum[i] == '-')
+            {
+                result += Convert.ToDouble(currentNum.ToString());
+                currentNum = new StringBuilder();
+            }
+            currentNum.Append(possibleUglyNum[i]);
+        }
+        result += Convert.ToDouble(currentNum.ToString());
+
+        if (Convert.ToDouble(result) == 0) return 1;
+        if (Convert.ToDouble(result) % 2 == 0) return 1;
+        if (Convert.ToDouble(result) % 3 == 0) return 1;
+        if (Convert.ToDouble(result) % 5 == 0) return 1;
+        if (Convert.ToDouble(result) % 7 == 0) return 1;
+        
+        return 0;
+    }
+
+    private static List<string> CreatePossibilities(List<List<string>> basePossibilities)
+    {
+        List<string> temp = basePossibilities[0];
+        
+        for (int i = 0; i < basePossibilities.Count - 1; i++)
+        {
+            temp = GetCartesianProduct(temp, basePossibilities[i + 1]);
+        }
+
+        return temp;
+    }
+
+    private static List<string> GetCartesianProduct(List<string> list1, List<string> list2)
+    {
+        var ret = new List<string>();
+                
+        var results = from possible1 in list1
+                        from possible2 in list2
+                        select new List<string>() { possible1 + possible2 };
+
+        foreach (var result in results)
+        {
+            ret.Add(result[0]);
+        }
+                
+        return ret;
+    }
+
+    private static List<List<string>> CreateBasePossibilities(string input)
+    {
+        var basePossiblities = new List<List<string>>();
+
+        //The first character doesn't get to have a + or - in front of it so we'll just add a list of 1 to it.
+        basePossiblities.Add(new List<string>() {input[0].ToString()});
+
+        for (int i = 1; i < input.Length; i++)
+        {
+            basePossiblities.Add(CreateNewPositionList(input[i].ToString()));
+        }
+
+        return basePossiblities;
+    }
+
+    private static List<string> CreateNewPositionList(string input)
+    {
+        return new List<string>() { input, "+" + input, "-" + input };
+    }
+}
---
+++ UglyNumbersTest/UglyNumbersTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UglyNumbersTest
+{
+    [TestClass]
+    public class UglyNumbersTest
+    {
+        PrivateType program;
+        [TestInitialize]
+        public void init()
+        {
+            program = new PrivateType(typeof(Program));
+        }
+        
+        [TestMethod]
+        public void CreateBasePossibilitiesTest()
+        {
+            var actual = (List<List<string>>)program.InvokeStatic("CreateBasePossibilities", "123");
+            Assert.AreEqual(1, actual[0].Count);
+            Assert.IsTrue(actual[1][1].Contains("+"));
+            Assert.IsTrue(actual[1][2].Contains("-"));
+            Assert.IsTrue(actual[2][1].Contains("+"));
+            Assert.IsTrue(actual[2][2].Contains("-"));
+
+            actual = (List<List<string>>)program.InvokeStatic("CreateBasePossibilities", "1");
+            Assert.AreEqual(1, actual.Count);
+
+            actual = (List<List<string>>)program.InvokeStatic("CreateBasePossibilities", "12345");
+            Assert.AreEqual(1, actual[0].Count);
+            Assert.IsTrue(actual[1][1].Contains("+"));
+            Assert.IsTrue(actual[1][2].Contains("-"));
+            Assert.IsTrue(actual[2][1].Contains("+"));
+            Assert.IsTrue(actual[2][2].Contains("-"));
+            Assert.IsTrue(actual[3][1].Contains("+"));
+            Assert.IsTrue(actual[3][2].Contains("-"));
+            Assert.IsTrue(actual[4][1].Contains("+"));
+            Assert.IsTrue(actual[4][2].Contains("-"));
+        }
+
+        [TestMethod]
+        public void CreatePossiblitiesTest()
+        {
+            var input = (List<List<string>>)program.InvokeStatic("CreateBasePossibilities", "12345");
+            var output = program.InvokeStatic("CreatePossibilities", new object [] {input});
+            Assert.IsTrue(((List<string>)output).Count == 81);
+        }
+
+        [TestMethod]
+        public void EvaluatesToUglyTest()
+        {
+            var inputUgly = "1+234-5+6"; //ugly
+            var inputNotUgly = "123+4-56"; //not ugly
+
+            var output = program.InvokeStatic("EvaluatesToUgly", inputUgly);
+            Assert.IsTrue(Convert.ToInt32(output) == 1, "Should be ugly");
+
+            output = program.InvokeStatic("EvaluatesToUgly", inputNotUgly);
+            Assert.IsTrue(Convert.ToInt32(output) == 0, "Should be not ugly");
+        }
+
+        [TestMethod]
+        public void FindUglyNumbersTest()
+        {
+            var output = program.InvokeStatic("FindUglyNumbers", "1");
+            Assert.IsTrue(Convert.ToInt32(output) == 0, "Output is " + output.ToString() + " should be 1");
+
+            output = program.InvokeStatic("FindUglyNumbers", "9");
+            Assert.IsTrue(Convert.ToInt32(output) == 1, "Output is " + output.ToString() + " should be 1");
+
+            output = program.InvokeStatic("FindUglyNumbers", "011");
+            Assert.IsTrue(Convert.ToInt32(output) == 6, "Output is " + output.ToString() + " should be 6");
+
+            output = program.InvokeStatic("FindUglyNumbers", "12345");
+            Assert.IsTrue(Convert.ToInt32(output) == 64, "Output is " + output.ToString() + " should be 64");
+
+            output = program.InvokeStatic("FindUglyNumbers", "3747766429");
+            Assert.IsTrue(Convert.ToInt32(output) == 14913, "Output is " + output.ToString() + " should be 14913");
+
+            var testInputs = new List<string>() {"0", "1","9","13","011","277","413","0876","7192","7329","8126","9893","12345","17214","20204","160176","1566843","9716463","54275347","84091195","91920744","216589640","845048810","3747766429","4802642960","8224853717","9712606483","0000000000277"};
+            foreach (var item in testInputs)
+            {
+                output = program.InvokeStatic("FindUglyNumbers", item);    
+            }
+            
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void GetCartesianProductTest()
+        {
+            var list1 = new List<string>() {"1"};
+            var list2 = new List<string>() {"+2", "-2", "2"};
+            
+            var actual = program.InvokeStatic("GetCartesianProduct", new object[] {list1, list2});
+            var expected = new List<string>() { "1+2", "1-2", "12" };
+
+            CollectionAssert.AreEqual(expected, (List<string>)actual);    
+        }
+    }
+}
